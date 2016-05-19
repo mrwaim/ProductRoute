a362d1e80a7e84f0577e333189f4b35402a792df
Klsandbox <ibrahim@klsandbox.com>
2016-05-19 10:27:00 +0800

    delivery

--- views/edit-product.blade.php
+++ views/edit-product.blade.php
@@ -18,7 +18,7 @@
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Product Name</label>
+                                <label class="col-md-2 control-label">Product Name</label>
 
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="name" value="{{ $product->name }}">
@@ -26,7 +26,7 @@
                             </div>
 
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Product Description</label>
+                                <label class="col-md-2 control-label">Product Description</label>
 
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="description"
@@ -34,18 +34,18 @@
                                 </div>
                             </div>
                             @if(! $config->group_enabled)
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">Product Price</label>
+                                <div class="form-group">
+                                    <label class="col-md-2 control-label">Product Price</label>
 
-                                <div class="col-md-6">
-                                    <input type="number" class="form-control" name="price"
-                                           value="{{ $product->productPricing->price }}">
+                                    <div class="col-md-6">
+                                        <input type="number" class="form-control" name="price"
+                                               value="{{ $product->productPricing->price }}">
+                                    </div>
                                 </div>
-                            </div>
                             @endif
 
                             <div class="form-group">
-                                <label for="image" class="control-label col-md-4">Product Image</label>
+                                <label for="image" class="control-label col-md-2">Product Image</label>
                                 <div class="col-md-6">
                                     <img width='40' height='40' src='{{asset($product->image)}}'>
                                     <input type="file" class="form-control" name="image" id="image">
@@ -53,7 +53,7 @@
                             </div> <!-- end div.form-group -->
 
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Bonus Category</label>
+                                <label class="col-md-2 control-label">Bonus Category</label>
 
                                 <div class="col-md-6">
                                     <select name='bonus_categories_id'>
@@ -68,48 +68,61 @@
                             </div>
 
                             @if($config->group_enabled)
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">Target Group</label>
-                                <div class="col-md-6 column">
-                                    <table class="table table-bordered table-hover" id="tab_logic">
-                                        <thead>
-                                        <tr>
-                                        <tr>
-                                            <th class="text-center" rowspan="2">Group</th>
-                                            <th class="text-center" colspan="2">Product Price</th>
-                                        </tr>
-                                        <tr>
-                                            <th class="text-center">West Malaysia</th>
-                                            <th class="text-center">East Malaysia</th>
-
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        <?php $index = 0 ?>
-                                        @foreach ($groups as $item)
-                                            <?php $index++ ?>
+                                <div class="form-group">
+                                    <label class="col-md-2 control-label">Target Group</label>
+                                    <div class="col-md-10 column">
+                                        <table class="table table-bordered table-hover" id="tab_logic">
+                                            <thead>
                                             <tr>
-                                                <input type="hidden" name="{{ "groups[{$index}][group_id]" }}"
-                                                       value="{{$item->id}}"/>
-                                                <input type="hidden" name="{{ "groups[{$index}][product_pricing_id]" }}"
-                                                       value="{{$product->pricingForGroup($item) ? $product->pricingForGroup($item)->id : 0}}"/>
-                                                <td>{{ $item->name }}</td>
-                                                <td>
-                                                <input type="number" name="{{ "groups[{$index}][price]" }}"
-                                                           value="{{$product->pricingForGroup($item) ? $product->pricingForGroup($item)->price : ''}}"
-                                                           placeholder='Price' class="form-control"/>
-                                                </td>
-                                                <td>
-                                                <input type="number" name="{{ "groups[{$index}][price_east]" }}"
-                                                           value="{{$product->pricingForGroup($item) ? $product->pricingForGroup($item)->price_east : ''}}"
-                                                           placeholder='Price' class="form-control"/>
-                                                </td>
+                                            <tr>
+                                                <th class="text-center" rowspan="2">Group</th>
+                                                <th class="text-center" colspan="4">Product Price</th>
+                                                <th class="text-center" colspan="4">Delivery</th>
                                             </tr>
-                                        @endforeach
-                                        </tbody>
-                                    </table>
+                                            <tr>
+                                                <th class="text-center" colspan="2">West Malaysia</th>
+                                                <th class="text-center" colspan="2">East Malaysia</th>
+                                                <th class="text-center" colspan="2">West Malaysia</th>
+                                                <th class="text-center" colspan="2">East Malaysia</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <?php $index = 0 ?>
+                                            @foreach ($groups as $item)
+                                                <?php $index++ ?>
+                                                <tr>
+                                                    <input type="hidden" name="{{ "groups[{$index}][group_id]" }}"
+                                                           value="{{$item->id}}"/>
+                                                    <input type="hidden"
+                                                           name="{{ "groups[{$index}][product_pricing_id]" }}"
+                                                           value="{{$product->pricingForGroup($item) ? $product->pricingForGroup($item)->id : 0}}"/>
+                                                    <td>{{ $item->name }}</td>
+                                                    <td colspan="2">
+                                                        <input type="number" name="{{ "groups[{$index}][price]" }}"
+                                                               value="{{$product->pricingForGroup($item) ? $product->pricingForGroup($item)->price : ''}}"
+                                                               placeholder='Price' class="form-control"/>
+                                                    </td>
+                                                    <td colspan="2">
+                                                        <input type="number" name="{{ "groups[{$index}][price_east]" }}"
+                                                               value="{{$product->pricingForGroup($item) ? $product->pricingForGroup($item)->price_east : ''}}"
+                                                               placeholder='Price' class="form-control"/>
+                                                    </td>
+                                                    <td colspan="2">
+                                                    <input type="number" name="{{ "groups[{$index}][delivery]" }}"
+                                                           value="{{$product->pricingForGroup($item) ? $product->pricingForGroup($item)->delivery : ''}}"
+                                                           placeholder='Delivery' class="form-control"/>
+                                                    </td>
+                                                    <td colspan="2">
+                                                        <input type="number" name="{{ "groups[{$index}][delivery_east]" }}"
+                                                               value="{{$product->pricingForGroup($item) ? $product->pricingForGroup($item)->delivery_east : ''}}"
+                                                               placeholder='Delivery' class="form-control"/>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
-                            </div>
                             @endif
 
                             <div class="form-group">
